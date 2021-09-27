b46d39ec5d3096c3afa4dd7df1c680c5be208f62
Alexandr Kryzhnii <cryzhny@gmail.com>
2021-09-27 21:25:28 +0300

    fix

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,9 +5,9 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-  const collator = Intl.Collator(undefined,{ caseFirst: 'upper' });
-  const arrCopy = [...arr]
-  const sorted = arrCopy.sort(collator.compare)
+  const collator = Intl.Collator(undefined, { caseFirst: 'upper' });
+  const arrCopy = [...arr];
+  const sorted = arrCopy.sort(collator.compare);
 
   return param === 'asc' ? sorted : sorted.reverse();
 }
