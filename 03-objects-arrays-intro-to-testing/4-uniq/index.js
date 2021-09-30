c113c3e66fdacadcb6f3ee9b5fb52e7bf4a4b841
Alexandr Kryzhnii <cryzhny@gmail.com>
2021-10-01 01:02:24 +0300

    homework 3 trim не осилил

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,11 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
+  const setArr = new Set(arr);
+  const result = [];
 
+  for (let item of setArr) {
+    result.push(item);
+  }
+  return result;
 }
