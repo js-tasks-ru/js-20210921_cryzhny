54e4e132bb38372e16ecda5d3fab32937e9b8ad9
Alexandr Kryzhnii <cryzhny@gmail.com>
2021-10-08 12:38:30 +0300

    сделал таблицу, нотификации не успел (было много работы) - доделаю на выходных

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,43 @@
 export default class NotificationMessage {
+  constructor(message = '', options = {}) {
+    this.message = message;
+    this.options = options;
+    this.open = false;
+  }
 
+  get template() {
+    const duration = this.options.duration && this.options.duration / 100;
+    return `
+      <div class="notification ${this.options.type}" style="--value:${duration}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.options.type}</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  show() {
+    this.open = true;
+    const element = document.createElement('div');
+
+    element.innerHTML = this.template;
+
+    this.element = element;
+    document.body.insertAdjacentHTML('beforeend', this.element.innerHTML);
+  }
+
+  remove() {
+    this.open = false;
+    const noty = document.querySelector('.notification')[0]
+    noty.parentElement.removeChild(noty);
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+  }
 }
